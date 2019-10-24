c2c2a23cb49abbcb1194263561d5a333bba58b1b
irisEco <549104586@qq.com>
2019-10-24 17:38:59 +0800

    添加 按command+return 发表评论的功劳

--- src/pages/matter/comment.js
+++ src/pages/matter/comment.js
@@ -1,12 +1,21 @@
 import React, { PureComponent } from 'react'
 import { MyCommentList } from './style'
-import {connect} from 'react-redux'
-import {actionCreater} from './store'
+import { connect } from 'react-redux'
+import { actionCreater } from './store'
 
 class Comment extends PureComponent {
 
+    componentDidMount() {
+        // 添加监听事件
+        document.addEventListener('keydowm', this.onKeyDown)
+    }
+    componentWillUnmount() {
+        //移出监听
+        document.removeEventListener('keydown', this.onKeyDown)
+    }
+
     render() {
-        const {handleSubmit,handleCancel} = this.props
+        const { handleSubmit, handleCancel ,handleKeyDown} = this.props
         return (
             <MyCommentList>
                 <div>
@@ -14,7 +23,8 @@ class Comment extends PureComponent {
                         <a className="avatar">
                             <img src="//upload.jianshu.io/users/upload_avatars/12343169/f4278717-a833-4217-8202-e61dd02d8d7b?imageMogr2/auto-orient/strip|imageView2/1/w/114/h/114/format/webp" />
                         </a>
-                        <textarea id="comment-content" placeholder="写下你的评论..." ></textarea>
+                        <textarea id="comment-content" placeholder="写下你的评论..."
+                            onKeyDown={(e) => handleKeyDown(e)} ></textarea>
                         <div className="write-function-block">
                             <div data-v-b36e9416="" className="emoji-modal-wrap">
                                 <a data-v-b36e9416="" className="emoji">
@@ -22,7 +32,7 @@ class Comment extends PureComponent {
                                 </a>
                             </div>
                             <div className="hint">⌘+Return 发表</div>
-                            <a className="btn btn-send" onClick={()=>handleSubmit()} >发送</a>
+                            <a className="btn btn-send" onClick={() => handleSubmit()} >发送</a>
                             <a className="cancel" onClick={this.handleCancel}>取消</a>
                         </div>
                     </form>
@@ -40,14 +50,29 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         //添加评论
-        handleSubmit(){
+        handleSubmit() {
             const comment = document.getElementById("comment-content");
             let nowTime = new Date()
             let time = nowTime.toLocaleString()
-            dispatch(actionCreater.AddComment(time,comment.value))  
+            dispatch(actionCreater.AddComment(time, comment.value))
             document.getElementById('comment-content').value = '';  //清空评论
 
         }
+        ,
+        //按command+return就发送
+        handleKeyDown(e) {
+            var keyCode = e.keyCode || e.which || e.charCode;
+            if (keyCode == 13 && (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)) {
+                const comment = document.getElementById("comment-content");
+                let nowTime = new Date()
+                let time = nowTime.toLocaleString()
+                dispatch(actionCreater.AddComment(time, comment.value))
+                document.getElementById('comment-content').value = '';  //清空评论
+                e.preventDefault();
+            }
+            // event.preventDefault(); // 注意：阻止默认事件不能放在外面，会阻止浏览器或者input/textarea的默认事件，应该放在相应的按键组合中去阻止
+            return false;
+        }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Comment);
+export default connect(mapStateToProps, mapDispatchToProps)(Comment);
